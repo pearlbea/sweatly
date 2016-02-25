9cc5d402e58e97c5b02a18e7d7c153ad73192428
Pearl Latteier <pearlbea@gmail.com>
2016-02-25 12:20:33 -0600

    adds title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	def tab_title(page_title = '')
+		base_title = 'Sweatly'
+		if page_title.empty?
+			base_title
+		else
+			page_title + ' | ' + base_title
+		end
+	end
+	
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Sweatly"
+    assert_select "title", "Sweatly"
   end
 
   test "should get help" do
